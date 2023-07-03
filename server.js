954a87b132447be4f841bef21f5f08ed49986d1c
Valerchik <new-house-drive@protonmail.com>
2023-07-04 02:24:12 +0300

    exclude logic with JSON fetch to separate folder

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-
-import fetch from "node-fetch";
-
-const shopname = "openicecat-live"
-const icecat_id = "56195846"
-const lang = "EN"
-let liveJSON = `https://live.icecat.biz/api?` +
-        `shopname=${shopname}`+
-        `&lang=${lang}`+
-        `&icecat_id=${icecat_id}`+
-        `&content=`
-
-const response = await fetch(liveJSON)
-const data = await response.json()
-
-console.log(data)
+import liveJSONHelper from "./icecatJSON/liveJSONHelper.js";
+const data = await liveJSONHelper.createJSONLink("openicecat-live", 56195846, "EN");
+console.log(data);
