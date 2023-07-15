e87fd52b19dca303b57be211fb74d316defe907a
Valerchik <new-house-drive@protonmail.com>
2023-07-15 10:20:18 +0300

    add express and root / handler

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-import liveJSONFetcher from "./icecatJSON/liveJSONFetcher.js";
-import liveJSONHelper from "./icecatJSON/liveJSONHelper.js";
 
-const product = await liveJSONFetcher.jsonGetProductByID("openicecat-live", 56195846, "EN");
-const usefulKeys = liveJSONHelper.getRootKeysByProductData(product)
+import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+import liveJSONFetcher from "./icecatJSON/liveJSONFetcher.js"; 
 
-console.log(usefulKeys)
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express()
+const PORT = 3000
+
+
+app.listen(PORT, () => {console.log("Proceed to PORT: " + PORT)})
+
+app.get('/', (req, res) => {
+    res.sendFile("index.html", {root: __dirname})
+})
+
+console.log(await liveJSONFetcher.jsonGetProductByID('valera-shop', "11771947", "EN"))
