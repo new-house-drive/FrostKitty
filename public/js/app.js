29990a4140bf3c9c6aad1bce360e8766b5351422
Valerchik <new-house-drive@protonmail.com>
2023-07-26 22:49:02 +0300

    create universal function with 7 args

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,47 @@ const getJSONButton = document.querySelector(".get-json-button")
 
 
 getJSONButton.onclick = () => {
+    // Essential information
     const inputShopname = document.getElementById("input-shopname").value
-    const inputIcecatID = document.getElementById("input-icecat-id").value
     const inputLang = document.getElementById('input-lang').value
-    let url = constructJSONLink(inputShopname, inputIcecatID, inputLang)
+    const inputAppKey = document.getElementById('input-app-key').value
+    
+    // Product Identifiers
+    const inputIcecatID = document.getElementById("input-icecat-id").value
+    const inputBrand = document.getElementById('input-brand').value
+    const inputMPN = document.getElementById('input-mpn').value
+    const inputGTIN = document.getElementById('input-gtin').value
+
+    // Product Identifiers Wrong Condition
+    if (!inputIcecatID && !inputGTIN) {
+      if (!inputMPN || !inputBrand) {
+        alert ('please use GTIN, ID or Brand + MPN pair to get the product')
+        return
+      }
+    }
+
+    let url = constructJSONLink(inputShopname, inputAppKey, inputIcecatID, inputLang, inputGTIN, inputMPN, inputBrand)
     window.open(url, '_blank') 
 };
 
-function constructJSONLink(shopname, icecat_id, lang) {
+
+//* One-to-rule-them-all JSON function.
+// input -> all fields from product form for JSON
+// Checks all the fields and add input as attributes
+function constructJSONLink(shopname, appkey, icecat_id, lang, gtin, mpn, brand, content) {
     const DOMAIN = `https://live.icecat.biz/api?`     
     
-    let url =
-      DOMAIN +
-      `shopname=${shopname}` +
-      `&lang=${lang}` +
-      `&icecat_id=${icecat_id}` +
-      `&content=`;
+    shopname = shopname || "openicecat-live"
+    let url = DOMAIN + `shopname=${shopname}&`
+
+    if (appkey) url += `app-key=${appkey}&`    
+    if (icecat_id) url+= `icecat_id=${icecat_id}&`
+    if (gtin) url+=`GTIN=${gtin}&`
+    if (brand) url+=`Brand=${brand}&`
+    if (mpn) url +=`ProductCode=${mpn}&`
+    
+    url += `lang=${lang}`;
+
     console.log("Your link is: " + url)
     return url;
   
